07402a3311751be9be31975ef050e1aa511a36f6
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-05-17 19:12:14 +0100

    Initialized .gitignore

---
+++ plot_basic.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+# use the .plot method to plot some coordinates 
+# here our parameters are 'x' and 'y' coordinates
+plt.plot([1,2,3],[5,7,4])
+
+plt.show() # draw to the screen
